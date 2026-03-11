ae65ca0d03c074924d378cf0135f4728589e4cf4
simonegreco5 <simoxey.greco@gmail.com>
2026-03-11 09:42:02 +0100

    add solution ex.1 saluto

--- snack-funzioni/01_saluti/script.js
+++ snack-funzioni/01_saluti/script.js
@@ -4,10 +4,18 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
+function saluto(nome) {
+    return 'ciao ' + nome
+}
 
+// scriviamo la stessa funzione usando una arrow function 
+let hi = (nome) => 'ciao ' + nome
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(saluto(userName));
+
+console.log(hi(userName));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario

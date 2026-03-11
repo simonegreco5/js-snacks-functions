d23c157dc8e04a2aa8e113d401f1ef8566432276
simonegreco5 <simoxey.greco@gmail.com>
2026-03-11 14:04:52 +0100

    add solution ex.5 bonus

--- snack-funzioni/05_bonus/script.js
+++ snack-funzioni/05_bonus/script.js
@@ -9,10 +9,40 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function saluto(nome) {
+    const data = new Date().getHours()
+
+    if (data >= 5 && data < 13) {
+        return 'buongiorno ' + nome
+
+    } else if (data >= 13 && data < 17) {
+        return 'buon pomeriggio ' + nome
+
+    } else if (data >= 17 && data < 5) {
+        return 'buonasera ' + nome
+    }
+}
+
+// scriviamo la stessa funzione usando una arrow function 
+let getSaluto = (nome) => {
+    const data = new Date().getHours()
+
+    if (data >= 5 && data < 13) {
+        return 'buongiorno ' + nome
+
+    } else if (data >= 13 && data < 17) {
+        return 'buon pomeriggio ' + nome
+
+    } else if (data >= 17 && data < 5) {
+        return 'buonasera ' + nome
+    }
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(saluto(name))
 
+console.log(getSaluto(name))
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.

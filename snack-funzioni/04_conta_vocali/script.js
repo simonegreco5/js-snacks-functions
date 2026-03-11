1beb0b921aa05332f65d307e070b729999af6244
simonegreco5 <simoxey.greco@gmail.com>
2026-03-11 11:41:50 +0100

    add solution ex.4 conta vocali

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -4,10 +4,41 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function splitVocali(name) {
+    const vocali = []
+    for (let i = 0; i < name.length; i++) {
+        
+        if (name[i] === 'a' || name[i] === 'e' || name[i] === 'i' || name[i] === 'o' || name[i] === 'u') {
+            vocali.push(name[i])
+        } else {
+            // non fare nulla
+        }
+        
+    }
 
+    return vocali
+}
+
+// scriviamo la stessa funzione usando una arrow function 
+let soloVocali = (parola) => {
+    const vocali = []
+    for (let i = 0; i < parola.length; i++) {
+        
+        if (parola[i] === 'a' || parola[i] === 'e' || parola[i] === 'i' || parola[i] === 'o' || parola[i] === 'u') {
+            vocali.push(parola[i])
+        } else {
+            // non fare nulla
+        }
+        
+    }
+
+    return vocali
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(splitVocali(word))
 
+console.log(soloVocali(word))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)

97d4cf455b0db758e77c55a0cff9229b4df20a5d
simonegreco5 <simoxey.greco@gmail.com>
2026-03-11 10:07:23 +0100

    add solution ex.2 iniziali

--- snack-funzioni/02_iniziali/script.js
+++ snack-funzioni/02_iniziali/script.js
@@ -4,10 +4,29 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function inizialeNome(name) {
+    const iniziale = []
+    for (let i = 0; i < name.length; i++) {
+        iniziale.push(name[i][0])
+        
+    }
+    return iniziale 
+}
+// scriviamo la stessa funzione usando una arrow function 
+let FirstWordName = (nome) => {
+    const firstWord = []
+    for (let i = 0; i < nome.length; i++) {
+        firstWord.push(nome[i][0])
+        
+    }
+    return firstWord 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(inizialeNome(names))
+
+console.log(FirstWordName(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]

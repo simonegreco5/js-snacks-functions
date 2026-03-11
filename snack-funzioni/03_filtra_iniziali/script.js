07049ee7b26d323cdc007dae1976fdf75612c186
simonegreco5 <simoxey.greco@gmail.com>
2026-03-11 11:15:37 +0100

    add solution ex.3 filtra iniziali

--- snack-funzioni/03_filtra_iniziali/script.js
+++ snack-funzioni/03_filtra_iniziali/script.js
@@ -4,10 +4,43 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filtroNomi(name) {
+    const nameList = []
+    for (let i = 0; i < name.length; i++) {
+
+        if (name[i][0] === 'A') {
+            nameList.push(name[i])
+        } else {
+            // non fare nulla 
+        }
+    }
+
+    return nameList
+}
+
+// scriviamo la stessa funzione usando una arrow function 
+let filtro = (nomi) => {
+    
+    const nomeLista = []
+    for (let i = 0; i < nomi.length; i++) {
+
+        if (nomi[i][0] === 'A') {
+            nomeLista.push(nomi[i])
+        } else {
+            // non fare nulla 
+        }
+    }
+
+    return nomeLista
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(filtroNomi(names))
+
+console.log(filtro(names));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
